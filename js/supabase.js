aa071d14b1bc8a2b0ff6dda88558de46b088f2ec
bhumikajangid7054-collab <bhumikajangid7054@gmail.com>
2026-07-29 17:28:09 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // Replace these with your actual project values
-const SUPABASE_URL = "https://tzabdsmfesbttzmmqtzn.supabase.co";
+const SUPABASE_URL = "https://tzabdsmfesbttzmmqtzn.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6YWJkc21mZXNidHR6bW1xdHpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzA0MzcsImV4cCI6MjEwMDY0NjQzN30.3yyWogU5ScNEX39DcpfI_dgtSIUGJ87WVo2kp_Tzwq8";
 
 // Create client
