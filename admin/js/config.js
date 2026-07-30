65531d28d3370fd94f425a431d6aeb2fd52e116a
bhumikajangid7054-collab <bhumikajangid7054@gmail.com>
2026-07-30 16:18:34 +0530

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -2,15 +2,16 @@
 // PRISM ADMIN CONFIG
 // ======================================
 
-// Your Supabase Project
-const SUPABASE_URL = "https://tzabdsmfesbttzmmqtzn.supabase.co";
+const SUPABASE_URL =
+"https://tzabdsmfesbttzmmqtzn.supabase.co";
 
-const SUPABASE_ANON_KEY = ""eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6YWJkc21mZXNidHR6bW1xdHpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzA0MzcsImV4cCI6MjEwMDY0NjQzN30.3yyWogU5ScNEX39DcpfI_dgtSIUGJ87WVo2kp_Tzwq8
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6YWJkc21mZXNidHR6bW1xdHpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzA0MzcsImV4cCI6MjEwMDY0NjQzN30.3yyWogU5ScNEX39DcpfI_dgtSIUGJ87WVo2kp_Tzwq8";
 
-// Create Supabase Client
-const { createClient } = window.supabase;
-
-const supabase = createClient(
+const supabase =
+window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+window.supabase = supabase;
