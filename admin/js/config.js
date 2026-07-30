3f761e777fbc5782697a8ca4b282a9eeb7b4e619
bhumikajangid7054-collab <bhumikajangid7054@gmail.com>
2026-07-30 16:21:45 +0530

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -8,10 +8,9 @@ const SUPABASE_URL =
 const SUPABASE_ANON_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6YWJkc21mZXNidHR6bW1xdHpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzA0MzcsImV4cCI6MjEwMDY0NjQzN30.3yyWogU5ScNEX39DcpfI_dgtSIUGJ87WVo2kp_Tzwq8";
 
-const supabase =
-window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
-window.supabase = supabase;
+window.supabaseClient = supabaseClient;
